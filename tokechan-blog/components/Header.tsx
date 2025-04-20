5416b20b8f4d5d089381e409bf325cb61ef15436
tokechan <fleatoke@gmail.com>
2025-04-20 19:53:33 +0900

    Add:CSS Modules

--- tokechan-blog/components/Header.tsx
+++ tokechan-blog/components/Header.tsx
@@ -1,11 +1,31 @@
+"use client";
+
 import Link from "next/link";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+    padding: 1rem 2rem;
+    background-color:rgb(7, 95, 117);
+    border-bottom: 1px solid #ddd;
+`;
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 1.5rem;
+    color: #fff;
+`;
 
 export default function Header() {
     return (
-        <header>
-            <nav>
-                <Link href="/">🏠Home</Link> | <Link href="/blog/list">💬Blog</Link>
-            </nav>
-        </header>
+        <>
+            <StyledHeader>
+                <nav>
+                    <StyledLink href="/">🏠Home</StyledLink> | <StyledLink href="/blog/list">💬Blog</StyledLink>
+                </nav>
+            </StyledHeader>       
+        </>
     );
 }
+
+ 
