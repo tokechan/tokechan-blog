65e30560d6f5968346888a467971c368544715c2
tokechan <fleatoke@gmail.com>
2025-04-20 20:54:41 +0900

    Add:Tag atmic

--- tokechan-blog/app/page.tsx
+++ tokechan-blog/app/page.tsx
@@ -1,14 +1,15 @@
 import { getPosts } from "@/lib/notion";
 import Link from "next/link";
+import styles from "./Home.module.css";
 
 export default async function Home() {
     const posts = await getPosts()
     return (
         <main style={{ padding: "2rem" }}>
-          <h1 style={{ fontSize: "5rem", fontWeight: "bold" }}>Tokechan Blog</h1>
+          <h1 style={{ fontSize: "4rem", fontWeight: "bold" }}>Tokechan Blog</h1>
 
   
-          <h1 style={{ fontSize: "2.5rem", color: "#0070f3" }}>こちらはポートフォリオ用のブログトップページです。</h1>
+          <h2 style={{ fontSize: "2.5rem", color: "rgb(115, 183, 201)" }}>こちらはTokeの💬ブログです。</h2>
         </main>
     )
 }
