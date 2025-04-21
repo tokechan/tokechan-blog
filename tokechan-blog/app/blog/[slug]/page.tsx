982040c86bd130a8830bf22d17648aa0e4392470
tokechan <fleatoke@gmail.com>
2025-04-21 17:41:14 +0900

    fix:Promis params

--- tokechan-blog/app/blog/[slug]/page.tsx
+++ tokechan-blog/app/blog/[slug]/page.tsx
@@ -4,58 +4,33 @@ import Link from "next/link";
 import { Breadcrumb } from "@/components/Breadcrumb";
 import styles from "./BlogPost.module.css";
 
-// export default async function BlogPostPage({ 
-//   params,
-// }: { 
-//   params: { slug: string } 
-// }) {
-//     const post = await getPostBySlug(params.slug);
-    
-//     if (!post) {
-//         return <div>記事が見つかりませんでした</div>;
-//     }
 
-//     return (
-//         <article className={styles.article}>
-//             <Breadcrumb items={[
-//                 { label: "ブログ一覧", href: "/blog/list" },
-//                 { label: post.title, href: `/blog/${params.slug}` },
-//             ]} />
-//             <h1 className={styles.title}>{post.title}</h1>
-//             <p className={styles.date}>{post.publishedDate}</p>
-//             <MarkdownWrapper html={String(post.content)} className={styles.markdown} />
-//             <Link href="/blog/list" className={styles.backButton}>ブログ一覧に戻る</Link>
-//         </article>
-//     );
-// }
-
-export default async function BlogPostPage({
+export default async function BlogPostPage( {
     params,
-  }: {
-    params: { slug: string };
-  }) {
-    const post = await getPostBySlug(params.slug);
-  
+ }:  {
+    params: Promise<{ slug: string }>;
+}) {
+    const { slug } = await params;
+    const post = await getPostBySlug(slug);
+    
     if (!post) {
-      return <div>記事が見つかりませんでした</div>;
+        return <div>記事が見つかりませんでした</div>;
     }
-  
+
     return (
-      <article className={styles.article}>
-        <Breadcrumb
-          items={[
-            { label: "ブログ一覧", href: "/blog/list" },
-            { label: post.title, href: `/blog/${params.slug}` },
-          ]}
-        />
-        <h1 className={styles.title}>{post.title}</h1>
-        <p className={styles.date}>{post.publishedDate}</p>
-        <MarkdownWrapper html={post.content} className={styles.markdown} />
-        <Link href="/blog/list" className={styles.backButton}>
-          ブログ一覧に戻る
-        </Link>
-      </article>
+        
+            <article className={styles.article}>
+            <Breadcrumb items={[
+                { label: "ブログ一覧", href: "/blog/list" },
+                { label: post.title, href: `/blog/${slug}` },
+            ]} />
+                <h1 className={styles.title}>{post.title}</h1>
+                <p className={styles.date}>{post.publishedDate}</p>
+                <MarkdownWrapper html={post.content} className={styles.markdown} />
+            
+            <Link href="/blog/list" className={styles.backButton}>ブログ一覧に戻る</Link>
+            </article>
+        
     );
-  }
-  
+}
 
