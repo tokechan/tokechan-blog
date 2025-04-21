9fbdcf59393f2755366ca814793d79d996c8e1b1
tokechan <fleatoke@gmail.com>
2025-04-21 15:09:17 +0900

    fix:eslint.config temporaly warn

--- tokechan-blog/app/page.tsx
+++ tokechan-blog/app/page.tsx
@@ -1,15 +1,14 @@
 import { getPosts } from "@/lib/notion";
-import Link from "next/link";
 import styles from "./Home.module.css";
 
 export default async function Home() {
-    const posts = await getPosts()
-    return (
-        <main style={{ padding: "2rem" }}>
-          <h1 style={{ fontSize: "4rem", fontWeight: "bold" }}>Tokechan Blog</h1>
+    // const posts = await getPosts()
 
-  
-          <h2 style={{ fontSize: "2.5rem", color: "rgb(115, 183, 201)" }}>こちらはTokeの💬ブログです。</h2>
+
+    return (
+        <main className={styles.main}>
+          <h1 className={styles.h1}>Tokechan Blog</h1>
+          <h2 className={styles.h2}>こちらはTokeの💬ブログです。</h2>
         </main>
     )
 }
